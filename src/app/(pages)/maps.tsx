11e95c0a6e46aa02fa53960bbedae0a56eea4a80
prampokan <prampokan@gmail.com>
2024-07-18 11:20:05 +0700

    fixing error in maps

--- src/app/(pages)/maps.tsx
+++ src/app/(pages)/maps.tsx
@@ -10,9 +10,9 @@ export default function Maps() {
                     width="100%" 
                     height="100%" 
                     style={{border:'0'}} 
-                    allowfullscreen="" 
+                    allowFullScreen
                     loading="lazy" 
-                    referrerpolicy="no-referrer-when-downgrade">
+                    referrerPolicy="no-referrer-when-downgrade">
                 </iframe>
             </div>
         </div>
