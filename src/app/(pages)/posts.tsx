c0c6072c969efc90353a444dc2be43084e4d176e
prampokan <prampokan@gmail.com>
2024-07-29 11:33:10 +0700

    fixing build error, removing window

--- src/app/(pages)/posts.tsx
+++ src/app/(pages)/posts.tsx
@@ -131,8 +131,6 @@ export default function Posts() {
         )
     }   
 
-    const isMobile = window.innerWidth <= 768;
-
     return (
         <div className="flex justify-center py-20 mt-20 px-5 xl:px-0">
             <div className="w-[70rem]">
@@ -146,7 +144,7 @@ export default function Posts() {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                                 </svg>
-                                {isMobile ? "Unggah foto!" : `Unggah Foto Sebagai ${user.displayName}`}
+                                Unggah Foto Sebagai {user.displayName}
                             </button>
                             <div className={`${modalOpen ? "opacity-100" : "invisible"} transition-all opacity-0 fixed top-0 left-0 w-full h-full flex justify-center items-center z-10 bg-black/50`}>
                                 <div className={`w-[22rem] h-[30rem] bg-white rounded-3xl overflow-hidden`}>
